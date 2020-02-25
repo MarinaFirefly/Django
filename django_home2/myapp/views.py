6f89f8b368ff1faeefffe6684215ebfd0d2ddb93
Laktionova <laktionova.marina@gmail.com>
2020-02-25 23:10:33 +0200

    finished homework2

--- django_home2/myapp/views.py
+++ django_home2/myapp/views.py
@@ -1,23 +1,28 @@
 import random
-from django.http import HttpResponseRedirect
-from django.urls import reverse
-from django.http import HttpResponse
+import string
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request,'index.html')
+    article_id = random.randint(1,3)
+    slug = ''
+    for i in range(0,10):
+        i = random.choice(string.ascii_lowercase + '-')
+        slug += i
+    return render(request,'index.html',{
+        'article_id':article_id,
+        'slug':slug,
+    })
 
 def first(request):
     return render(request,'first.html')
 
 def article(request,article_id):
-    article_id = random.randint(1,3)
     return render(request,f'articles/{article_id}.html',{
         'article_id':article_id,
     })
 
 def slug_rand(request,slug):
-    slug = 'qw'
     return render(request,'article.html',{
         'slug':slug
     })
